8266914f35a6d58823c56738db330424ac77317d
Vincent <vincent@zcorrecteurs.fr>
2012-05-15 20:22:31 +0200

    Crée les fichiers de configuration pour les tests si nécessaire.

--- app/bootstrap_tests.php
+++ app/bootstrap_tests.php
@@ -3,4 +3,13 @@
 define('BASEPATH', realpath(__DIR__.'/..'));
 define('APP_PATH', BASEPATH.'/app');
 
+if (!is_file(__DIR__.'/config/parameters.yml'))
+{
+	copy(__DIR__.'/config/parameters.sample.yml', __DIR__.'/config/parameters.yml');
+}
+if (!is_file(__DIR__.'/config/constants.yml'))
+{
+	copy(__DIR__.'/config/constants.sample.yml', __DIR__.'/config/constants.yml');
+}
+
 require_once __DIR__.'/bootstrap.php.cache';

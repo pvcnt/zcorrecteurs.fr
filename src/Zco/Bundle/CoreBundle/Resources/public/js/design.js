5650c73fddb9e1223d4612b42b8c120ea68d37ef
Vincent <vincent@zcorrecteurs.fr>
2012-07-28 16:17:57 +0200

    Implémente un easter-egg !

--- src/Zco/Bundle/CoreBundle/Resources/public/js/design.js
+++ src/Zco/Bundle/CoreBundle/Resources/public/js/design.js
@@ -1,30 +1,53 @@
 /**
- * @requires jquery-no-conflict
- *           @ZcoCoreBundle/Resources/public/js/libs/bootstrap.js
- */
+  * @requires jquery-no-conflict
+  *           bootstrap-js
+  *           @ZcoCoreBundle/Resources/public/js/libs/zMessage.js
+  */
+jQuery(function() {
+	var win = jQuery(window);
 
-var win = jQuery(window);
-var body = jQuery('body');
-var nav = jQuery('div.navbar');
-var navTop = jQuery('.navbar-inner').offset().top;
-var isFixed = 0;
+	//Rend la barre de navigation fixe en haut de page dès qu'elle devrait 
+	//devenir cachée.
+	
+	var body = jQuery('body');
+	var nav = jQuery('div.navbar');
+	var navTop = jQuery('.navbar-inner').offset().top;
+	var isFixed = 0;
 
-processScroll();
-win.on('scroll', processScroll);
-
-function processScroll()
-{
-	var scrollTop = win.scrollTop();
-	if (scrollTop >= navTop && !isFixed)
+	var processScroll = function()
 	{
-		isFixed = 1;
-		nav.addClass('navbar-fixed-top');
-		body.css('margin-top', 40);
-	}
-	else if (scrollTop <= navTop && isFixed)
-	{
-		isFixed = 0;
-		nav.removeClass('navbar-fixed-top');
-		body.css('margin-top', 0);
-	}
-}
+		var scrollTop = win.scrollTop();
+		if (scrollTop >= navTop && !isFixed) {
+			isFixed = 1;
+			nav.addClass('navbar-fixed-top');
+			body.css('margin-top', 40);
+		} else if (scrollTop <= navTop && isFixed) {
+			isFixed = 0;
+			nav.removeClass('navbar-fixed-top');
+			body.css('margin-top', 0);
+		}
+	};
+
+	processScroll();
+	win.on('scroll', processScroll);
+
+	//Active le code Konami. Yeah !
+    var konami = [];
+    win.on('keydown', function(event){
+        konami.push(event.keyCode);
+        if (konami.toString().indexOf("38,38,40,40,37,39,37,39,66,65") >= 0) {
+        	zMessage.info('Bon jeu !');
+            var script = document.createElement("script");
+			script.setAttribute('type', 'text/javascript');
+			script.onreadystatechange = function() {
+				if (script.readyState == 'loaded' || script.readyState == 'complete') {
+					alert('loaded');
+				}
+			};
+			script.src = "/bundles/zcocore/js/vendor/asteroids.js?v=3";
+			document.getElementsByTagName('head')[0].appendChild(script);
+
+            konami = [];
+        }
+	});
+});

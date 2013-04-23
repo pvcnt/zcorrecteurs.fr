f2db6e2d2ec93644cb2fff609aaee2508da06f6b
Vincent <vincent@zcorrecteurs.fr>
2013-04-23 08:29:15 +0200

    Corrige #973 : Retire le lien vers le SdZ dans le pied de page.

--- app/Resources/views/layouts/bootstrap.html.php
+++ app/Resources/views/layouts/bootstrap.html.php
@@ -1,105 +1,100 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta http-equiv="Content-Language" content="fr" />
-		<meta name="description" content="<?php echo Page::$description; ?>" />
-		<meta name="robots" content="<?php echo Page::$robots; ?>" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="fr" />
+        <meta name="description" content="<?php echo Page::$description; ?>" />
+        <meta name="robots" content="<?php echo Page::$robots; ?>" />
 
-		<title><?php echo str_replace(array(' '), ' ', Page::$titre); ?></title>
-		
-		<?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/css/design.css') ?>
-		<?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/js/design.js') ?>
-        
-		<?php foreach ($view['vitesse']->stylesheets() as $assetUrl): ?>
-		    <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
-		<?php endforeach ?>
-    	
-		<?php foreach ($view['vitesse']->javascripts(array('mootools', 'mootools-more')) as $assetUrl): ?>
-		    <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
-		<?php endforeach ?>
-		
-		<?php echo $view['vitesse']->renderFeeds() ?>
-		
-		<link rel="icon" type="image/png" href="/favicon.png" />
-		<link rel="start" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !" href="/" />
-	</head>
+        <title><?php echo str_replace(array(' '), ' ', Page::$titre); ?></title>
 
-	<body>
-		<?php if (empty($xhr)): ?>
-	    <div id="header">
-			<div id="header-oreilles">
-				<a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
-					zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
-				</a>
-			</div>
-			<div id="header-zcorrecteurs">
-				<a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
-					zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
-				</a>
-			</div>
+        <?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/css/design.css') ?>
+        <?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/js/design.js') ?>
 
-			<?php echo $view['ui']->headerRight('bootstrap') ?>
-		</div> <!-- /header -->
+        <?php foreach ($view['vitesse']->stylesheets() as $assetUrl): ?>
+            <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
+        <?php endforeach ?>
 
-		<div class="navbar navbar-inverse">
-			<div class="navbar-inner">
-				<div class="container">
-					<?php echo $view['ui']->speedbarre('bootstrap') ?>
-					<?php echo $view['ui']->speedbarreRight('bootstrap') ?>
-		    	</div>
-			</div>
-		</div> <!-- /navbar -->
-		<?php endif ?>
-			
-		<div class="container-fluid<?php if (empty($xhr)): ?> main-container<?php endif ?>">
-			<div class="row-fluid">
-				<?php if (empty($xhr)): ?>
-			    <div class="span2 sidebar">
-			        <?php echo $view['ui']->leftMenu('bootstrap') ?>
-			    </div>
-				<?php endif ?>
-			    
-			    <div class="<?php echo empty($xhr) ? 'span10' : 'span12' ?> content">
-			        <?php echo $view->render('::layouts/flashes.html.php', compact('maintenance')) ?>
-			
-					<?php if (empty($xhr)): ?>
-	        			<div id="postloading-area"></div>
+        <?php foreach ($view['vitesse']->javascripts(array('mootools', 'mootools-more')) as $assetUrl): ?>
+            <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
+        <?php endforeach ?>
 
-	        			<?php echo $view['ui']->breadcrumb('bootstrap') ?>
-        			<?php endif ?>
-			
-        			<?php $view['slots']->output('_content') ?>
-			    </div>
-			</div> <!-- /row-fluid -->
-		</div> <!-- /container-fluid -->
-		
-		<?php if (empty($xhr)): ?>
-		<div id="footer">
-			<div class="left">
-			    <span>Site fièrement édité par</span>
-				<a href="http://www.corrigraphie.org" title="Ce site est hébergé et édité par l’association Corrigraphie.">Corrigraphie</a>
-			</div>
-			
-			<div class="right bloc_partenaires">
-				<a href="http://www.siteduzero.com" title="Le Site du Zéro, site communautaire de tutoriels gratuits pour débutants : programmation, création de sites Web, Linux..." id="pub-19">Site du Zéro</a>
-				<?php $view['cache']->increment('pub_nbv-19') ?>
-			</div>
-			
-			<div class="center">
-				<p><a href="#" title="Remonter en haut de page" id="toplink">Haut de page</a></p>
-				
-				<?php echo $view['ui']->footer(1); ?>
-				<?php echo $view['ui']->footer(2); ?>
-				<?php echo $view['ui']->footer(3, array('childrenAttributes' => array('class' => 'links bloc_partenaires'), 'preHtml' => 'Partenaires : ')); ?>
-			</div>
-		</div>
-		<?php endif ?>
-	
-		<?php foreach ($view['vitesse']->javascripts() as $assetUrl): ?>
-		    <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
-		<?php endforeach ?>	
-		<script type="text/javascript" src="<?php echo $view['router']->generate('fos_js_routing_js', array('callback' => 'fos.Router.setData')) ?>"></script>	
-		<?php echo $view['javelin']->renderHTMLFooter() ?>
-	</body>
+        <?php echo $view['vitesse']->renderFeeds() ?>
+
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="start" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !" href="/" />
+    </head>
+
+    <body>
+        <?php if (empty($xhr)): ?>
+            <div id="header">
+                <div id="header-oreilles">
+                    <a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
+                        zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
+                    </a>
+                </div>
+                <div id="header-zcorrecteurs">
+                    <a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
+                        zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
+                    </a>
+                </div>
+
+                <?php echo $view['ui']->headerRight('bootstrap') ?>
+            </div> <!-- /header -->
+
+            <div class="navbar navbar-inverse">
+                <div class="navbar-inner">
+                    <div class="container">
+                        <?php echo $view['ui']->speedbarre('bootstrap') ?>
+                        <?php echo $view['ui']->speedbarreRight('bootstrap') ?>
+                    </div>
+                </div>
+            </div> <!-- /navbar -->
+        <?php endif ?>
+
+        <div class="container-fluid<?php if (empty($xhr)): ?> main-container<?php endif ?>">
+            <div class="row-fluid">
+                <?php if (empty($xhr)): ?>
+                    <div class="span2 sidebar">
+                        <?php echo $view['ui']->leftMenu('bootstrap') ?>
+                    </div>
+                <?php endif ?>
+
+                <div class="<?php echo empty($xhr) ? 'span10' : 'span12' ?> content">
+                    <?php echo $view->render('::layouts/flashes.html.php', compact('maintenance')) ?>
+
+                    <?php if (empty($xhr)): ?>
+                        <div id="postloading-area"></div>
+
+                        <?php echo $view['ui']->breadcrumb('bootstrap') ?>
+                    <?php endif ?>
+
+                    <?php $view['slots']->output('_content') ?>
+                </div>
+            </div> <!-- /row-fluid -->
+        </div> <!-- /container-fluid -->
+
+        <?php if (empty($xhr)): ?>
+            <div id="footer">
+                <div class="left">
+                    <span>Site fièrement édité par</span>
+                    <a href="http://www.corrigraphie.org" title="Ce site est hébergé et édité par l’association Corrigraphie.">Corrigraphie</a>
+                </div>
+
+                <div class="center">
+                    <p><a href="#" title="Remonter en haut de page" id="toplink">Haut de page</a></p>
+
+                    <?php echo $view['ui']->footer(1); ?>
+                    <?php echo $view['ui']->footer(2); ?>
+                    <?php echo $view['ui']->footer(3, array('childrenAttributes' => array('class'   => 'links bloc_partenaires'), 'preHtml' => 'Partenaires : ')); ?>
+                </div>
+            </div>
+        <?php endif ?>
+
+        <?php foreach ($view['vitesse']->javascripts() as $assetUrl): ?>
+            <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
+        <?php endforeach ?>	
+        <script type="text/javascript" src="<?php echo $view['router']->generate('fos_js_routing_js', array('callback' => 'fos.Router.setData')) ?>"></script>	
+        <?php echo $view['javelin']->renderHTMLFooter() ?>
+    </body>
 </html>
--- app/Resources/views/layouts/default.html.php
+++ app/Resources/views/layouts/default.html.php
@@ -1,166 +1,161 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<meta http-equiv="Content-Language" content="fr" />
-		<meta name="language" content="fr" />
-		<meta http-equiv="content-language" content="fr" />
-		<meta name="description" content="<?php echo Page::$description; ?>" />
-		<meta name="robots" content="<?php echo Page::$robots; ?>" />
-		<?php $view['slots']->output('meta') ?>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Language" content="fr" />
+        <meta name="language" content="fr" />
+        <meta http-equiv="content-language" content="fr" />
+        <meta name="description" content="<?php echo Page::$description; ?>" />
+        <meta name="robots" content="<?php echo Page::$robots; ?>" />
+        <?php $view['slots']->output('meta') ?>
 
-		<title><?php echo str_replace(array(' '), ' ', Page::$titre); ?></title>
-		
-		<?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/css/global.css') ?>
-		<?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/js/global.js') ?>
-        
-		<?php foreach ($view['vitesse']->stylesheets() as $assetUrl): ?>
-		    <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
-		<?php endforeach ?>
-		<!--[if IE]>
+        <title><?php echo str_replace(array(' '), ' ', Page::$titre); ?></title>
+
+        <?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/css/global.css') ?>
+        <?php $view['vitesse']->requireResource('@ZcoCoreBundle/Resources/public/js/global.js') ?>
+
+        <?php foreach ($view['vitesse']->stylesheets() as $assetUrl): ?>
+            <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
+        <?php endforeach ?>
+        <!--[if IE]>
         <?php foreach ($view['vitesse']->stylesheets(array('@ZcoCoreBundle/Resources/public/css/ie.css')) as $assetUrl): ?>
-		    <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
-		<?php endforeach ?>
-    	<![endif]-->
-    	
-		<?php foreach ($view['vitesse']->javascripts(array('mootools', 'mootools-more')) as $assetUrl): ?>
-		    <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
-		<?php endforeach ?>
-		
-		<?php echo $view['vitesse']->renderFeeds() ?>
-		
-		<link rel="icon" type="image/png" href="/favicon.png" />
-		<link rel="start" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !" href="/" />
-	</head>
+                <link rel="stylesheet" href="<?php echo $assetUrl ?>" media="screen" type="text/css" />
+        <?php endforeach ?>
+        <![endif]-->
 
-	<body>
-		<div id="acces_rapide">
-		    <a href="#page">Aller au menu</a> - 
-		    <a href="#content">Aller au contenu</a>
-		</div>
+        <?php foreach ($view['vitesse']->javascripts(array('mootools', 'mootools-more')) as $assetUrl): ?>
+            <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
+        <?php endforeach ?>
 
-		<div id="body">
-		    <?php if (empty($xhr)): ?>
-			<div id="header">
-				<div id="title">
-					<div id="title-oreilles">
-						<a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
-							zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
-						</a>
-					</div>
-					<div id="title-zcorrecteurs">
-						<a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
-							zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
-						</a>
-					</div>
-					
-					<?php echo $view['ui']->headerRight('legacy') ?>
-				</div>
-			</div>
-			<div id="speedbarre">
-				<?php echo $view['ui']->speedbarre('legacy') ?>
-			</div>
-			
-			<div id="page">
-				<div class="sidebar sidebarleft"><?php echo $view['ui']->leftMenu('legacy') ?></div>
-			<div id="content" class="right">
-			<?php endif ?>
-			<?php
-			/* Affichage de l'éventuel message de maintenance */
-			if ($maintenance)
-			{
-				afficher_erreur('Attention, le site est actuellement en maintenance !');
-			}
+        <?php echo $view['vitesse']->renderFeeds() ?>
 
-			/* Affichage des messages éventuels en haut de la page */
-			if (!empty($_SESSION['erreur']))
-			{
-				foreach($_SESSION['erreur'] as $erreur)
-					afficher_erreur($erreur);
-				$_SESSION['erreur'] = array();
-			}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="start" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !" href="/" />
+    </head>
 
-			if (!empty($_SESSION['message']))
-			{
-				foreach($_SESSION['message'] as $message)
-					afficher_message($message);
-				$_SESSION['message'] = array();
-			}
+    <body>
+        <div id="acces_rapide">
+            <a href="#page">Aller au menu</a> - 
+            <a href="#content">Aller au contenu</a>
+        </div>
 
-			/* Affichage de l'admin rapide éventuellement */
-			if(empty($xhr) && verifier('admin') && preference('display_admin_bar') == 1){ ?>
-			<div id="admin_rapide">
-				<?php if(verifier('groupes_gerer')){ ?>
-				<div class="admin_lien">
-					<a href="/groupes/">
-						<img src="/pix.gif" class="fff group" alt="" /> Groupes
-					</a>
-				</div>
-				<?php } if(verifier('blog_voir_billets_proposes')){ $billets = \Container::getService('zco_admin.manager')->get('blog'); ?>
-				<div class="admin_lien">
-					<a href="/blog/propositions.html"<?php if($billets > 0) echo ' title="'.$billets.' billet'.pluriel($billets).' en attente"'; ?>>
-						<img src="/pix.gif" class="fff book" alt="" />
-						<?php if($billets > 0) echo '<strong>'; ?>Blog
-						<?php if($billets > 0) echo '('.$billets.')</strong>'; ?>
-					</a>
-				</div>
-				<?php } if(verifier('recrutements_voir_candidatures')){ $candidatures = \Container::getService('zco_admin.manager')->get('recrutement'); ?>
-				<div class="admin_lien">
-					<a href="/recrutement/gestion.html"<?php if($candidatures > 0) echo ' title="'.$candidatures.' candidature'.pluriel($candidatures).' en attente"'; ?>>
-						<img src="/pix.gif" class="fff door_in" alt="" />
-						<?php if($candidatures > 0) echo '<strong>'; ?>Recrutements
-						<?php if($candidatures > 0) echo '('.$candidatures.')</strong>'; ?>
-					</a>
-				</div>
-				<?php } if(verifier('zcorriger') || verifier('voir_tutos_attente')){ $tutos = \Container::getService('zco_admin.manager')->get('zcorrection'); ?>
-				<div class="admin_lien">
-					<a href="/zcorrection/"<?php if($tutos > 0) echo ' title="'.$tutos.' tutoriel'.pluriel($tutos).' en attente"'; ?>>
-						<img src="/pix.gif" class="fff pencil_green" alt="" />
-						<?php if($tutos > 0) echo '<strong>'; ?>zCorrection
-						<?php if($tutos > 0) echo '('.$tutos.')</strong>'; ?>
-					</a>
-				</div>
-				<?php } ?>
-			</div>
-			<?php } ?>
-			
-			<?php if (empty($xhr)): ?>
-			<div id="postloading-area"></div>
-			
-			<?php echo $view['ui']->breadcrumb('legacy') ?>
-		    <?php endif ?>
-			
-			<?php $view['slots']->output('_content') ?>
-
+        <div id="body">
             <?php if (empty($xhr)): ?>
-			</div>
-			</div>
-			<div id="footer">
-				<div class="left">
-				    <span>Site fièrement édité par</span>
-					<a href="http://www.corrigraphie.org" title="Ce site est hébergé et édité par l’association Corrigraphie.">Corrigraphie</a>
-				</div>
-				
-				<div class="right bloc_partenaires">
-					<a href="http://www.siteduzero.com" title="Le Site du Zéro, site communautaire de tutoriels gratuits pour débutants : programmation, création de sites Web, Linux..." id="pub-19">Site du Zéro</a>
-					<?php $view['cache']->increment('pub_nbv-19') ?>
-				</div>
-				
-				<div class="center">
-					<p><a href="#" title="Remonter en haut de page" id="toplink">Haut de page</a></p>
-					
-					<?php echo $view['ui']->footer(1); ?>
-					<?php echo $view['ui']->footer(2); ?>
-					<?php echo $view['ui']->footer(3, array('childrenAttributes' => array('class' => 'links bloc_partenaires'), 'preHtml' => 'Partenaires : ')); ?>
-				</div>
-			</div>
-		    <?php endif ?>
-		
-			<?php foreach ($view['vitesse']->javascripts() as $assetUrl): ?>
-    		    <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
-    		<?php endforeach ?>
-			<script type="text/javascript" src="<?php echo $view['router']->generate('fos_js_routing_js', array('callback' => 'fos.Router.setData')) ?>"></script>
-			<?php echo $view['javelin']->renderHTMLFooter() ?>
-		</div>
-	</body>
+                <div id="header">
+                    <div id="title">
+                        <div id="title-oreilles">
+                            <a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
+                                zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
+                            </a>
+                        </div>
+                        <div id="title-zcorrecteurs">
+                            <a href="/" title="zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !">
+                                zCorrecteurs.fr - Les réponses à toutes vos questions concernant la langue française !
+                            </a>
+                        </div>
+
+                        <?php echo $view['ui']->headerRight('legacy') ?>
+                    </div>
+                </div>
+                <div id="speedbarre">
+                    <?php echo $view['ui']->speedbarre('legacy') ?>
+                </div>
+
+                <div id="page">
+                    <div class="sidebar sidebarleft"><?php echo $view['ui']->leftMenu('legacy') ?></div>
+                    <div id="content" class="right">
+                    <?php endif ?>
+                    <?php
+                    /* Affichage de l'éventuel message de maintenance */
+                    if ($maintenance) {
+                        afficher_erreur('Attention, le site est actuellement en maintenance !');
+                    }
+
+                    /* Affichage des messages éventuels en haut de la page */
+                    if (!empty($_SESSION['erreur'])) {
+                        foreach ($_SESSION['erreur'] as $erreur)
+                            afficher_erreur($erreur);
+                        $_SESSION['erreur'] = array();
+                    }
+
+                    if (!empty($_SESSION['message'])) {
+                        foreach ($_SESSION['message'] as $message)
+                            afficher_message($message);
+                        $_SESSION['message'] = array();
+                    }
+
+                    /* Affichage de l'admin rapide éventuellement */
+                    if (empty($xhr) && verifier('admin') && preference('display_admin_bar') == 1) {
+                        ?>
+                        <div id="admin_rapide">
+                            <?php if (verifier('groupes_gerer')) { ?>
+                                <div class="admin_lien">
+                                    <a href="/groupes/">
+                                        <img src="/pix.gif" class="fff group" alt="" /> Groupes
+                                    </a>
+                                </div>
+                            <?php } if (verifier('blog_voir_billets_proposes')) {
+                                $billets = \Container::getService('zco_admin.manager')->get('blog'); ?>
+                                <div class="admin_lien">
+                                    <a href="/blog/propositions.html"<?php if ($billets > 0) echo ' title="' . $billets . ' billet' . pluriel($billets) . ' en attente"'; ?>>
+                                        <img src="/pix.gif" class="fff book" alt="" />
+                                        <?php if ($billets > 0) echo '<strong>'; ?>Blog
+                                        <?php if ($billets > 0) echo '(' . $billets . ')</strong>'; ?>
+                                    </a>
+                                </div>
+                            <?php } if (verifier('recrutements_voir_candidatures')) {
+                                $candidatures = \Container::getService('zco_admin.manager')->get('recrutement'); ?>
+                                <div class="admin_lien">
+                                    <a href="/recrutement/gestion.html"<?php if ($candidatures > 0) echo ' title="' . $candidatures . ' candidature' . pluriel($candidatures) . ' en attente"'; ?>>
+                                        <img src="/pix.gif" class="fff door_in" alt="" />
+                                        <?php if ($candidatures > 0) echo '<strong>'; ?>Recrutements
+                                        <?php if ($candidatures > 0) echo '(' . $candidatures . ')</strong>'; ?>
+                                    </a>
+                                </div>
+                            <?php } if (verifier('zcorriger') || verifier('voir_tutos_attente')) {
+                                $tutos = \Container::getService('zco_admin.manager')->get('zcorrection'); ?>
+                                <div class="admin_lien">
+                                    <a href="/zcorrection/"<?php if ($tutos > 0) echo ' title="' . $tutos . ' tutoriel' . pluriel($tutos) . ' en attente"'; ?>>
+                                        <img src="/pix.gif" class="fff pencil_green" alt="" />
+                                        <?php if ($tutos > 0) echo '<strong>'; ?>zCorrection
+                                        <?php if ($tutos > 0) echo '(' . $tutos . ')</strong>'; ?>
+                                    </a>
+                                </div>
+                        <?php } ?>
+                        </div>
+                    <?php } ?>
+
+                    <?php if (empty($xhr)): ?>
+                        <div id="postloading-area"></div>
+                        <?php echo $view['ui']->breadcrumb('legacy') ?>
+                    <?php endif ?>
+
+                    <?php $view['slots']->output('_content') ?>
+
+                    <?php if (empty($xhr)): ?>
+                    </div>
+                </div>
+                <div id="footer">
+                    <div class="left">
+                        <span>Site fièrement édité par</span>
+                        <a href="http://www.corrigraphie.org" title="Ce site est hébergé et édité par l’association Corrigraphie.">Corrigraphie</a>
+                    </div>
+
+                    <div class="center">
+                        <p><a href="#" title="Remonter en haut de page" id="toplink">Haut de page</a></p>
+
+                        <?php echo $view['ui']->footer(1); ?>
+                        <?php echo $view['ui']->footer(2); ?>
+                        <?php echo $view['ui']->footer(3, array('childrenAttributes' => array('class'   => 'links bloc_partenaires'), 'preHtml' => 'Partenaires : ')); ?>
+                    </div>
+                </div>
+            <?php endif ?>
+
+            <?php foreach ($view['vitesse']->javascripts() as $assetUrl): ?>
+                <script type="text/javascript" src="<?php echo $assetUrl ?>"></script>
+            <?php endforeach ?>
+            <script type="text/javascript" src="<?php echo $view['router']->generate('fos_js_routing_js', array('callback' => 'fos.Router.setData')) ?>"></script>
+            <?php echo $view['javelin']->renderHTMLFooter() ?>
+        </div>
+    </body>
 </html>
